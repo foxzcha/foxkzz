9ed2dfadf16dd5d3f2c0642e7e19033fb4087142
Foxzz <145376915+foxzcha@users.noreply.github.com>
2024-08-10 17:27:05 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,27 @@
+//Foxzz Page
+function foxzz() {
+    window.location.href="./Foxzz/index.html"
+}
+
+//Depression
+function depre() {
+    window.location.href="./Lies/l.html"
+}
+
+//UAS
+function UAS() {
+    window.location.href="./uas/index.html"
+}
+
+//Out page massage
+let docTitle = document.title;
+window.addEventListener("blur", () =>{
+    document.title = "Yo bro";
+})
+window.addEventListener("focus", () =>{
+    document.title = docTitle;
+})
+
+
+// Foxzz Developments
+// All Copyrights are reserverds. 2020
